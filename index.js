e754e186da21a580f46f3b7f90bc03a2800fdfc8
Gabriel Onike <onikegabriel@gmail.com>
2019-09-05 10:59:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-function getFirstSelector(s) {
-  return document.querySelector(s);
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
 }
 
 function nestedTarget() {
   return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (i=0, l=rankedLists.length ; i<l; i++){
+    let children = rankedLists[i].children ;
+
+    for ( j=0, l= children.length; j<l; j++){
+      let children[j].innerHTML = parseInt(children[j].innerHTML + n);
+    }
+  }
 }
